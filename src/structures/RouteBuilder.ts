67164e4111708a840301d5f9aed12f2e91544429
Andrew Tandy <andrewtandy12@gmail.com>
2025-04-22 20:33:02 -0700

    feat: :sparkles: optional request validation with Joi schemas
    You can now use built-in schema support and automatic request validation

--- src/structures/RouteBuilder.ts
+++ src/structures/RouteBuilder.ts
@@ -1,12 +1,15 @@
 import EventEmitter from '@3xpo/events';
+import Joi from 'joi';
 
 import type { Route, RouteEvent, HttpMethod, RouteHandler, } from '../interfaces/route';
+import type { ValidationSchemas, } from '../interfaces/validationSchemas';
 import type { middleware, } from '../interfaces/middleware';
 
 
 export default class RouteBuilder extends (EventEmitter as unknown as new () => Route) {
   public supportedMethods: Array<HttpMethod>;
   public middleware?: middleware;
+  public schemas: Partial<Record<HttpMethod, ValidationSchemas>> = {};
 
   /**
    * @param {middleware} [middleware] - Optional middleware to be used with the route.
@@ -16,6 +19,50 @@ export default class RouteBuilder extends (EventEmitter as unknown as new () => 
 
     this.supportedMethods = [];
     this.middleware = middleware;
+  };
+
+  /**
+   * Middleware to validate request data against schemas.
+   * @param method - The HTTP method.
+   * @returns Middleware function.
+  */
+  private _validateSchema(method: HttpMethod) {
+    return (request, response, next) => {
+      const schema = this.schemas[method];
+      
+      if (schema === undefined) 
+        return next();
+
+      try {
+        for (const [key, validator] of Object.entries(schema) as [keyof typeof schema, any][]) {
+          const { error, value, } = validator.validate(request[key]);
+
+          if (error) 
+            throw new Error(error.message);
+
+          request[key] = value;
+        };
+
+        next();
+      } catch (error) {
+        response.status(400).json({ 'error': error.message, });
+      };
+    };
+  };
+
+  /**
+   * Defines validation schemas for a specific HTTP method.
+   * @param {HttpMethod} method - The HTTP method to define schemas for.
+   * @param {(schema: typeof Joi) => ValidationSchemas} schemaCallback - A callback to define schemas.
+   * @returns {this} The RouteBuilder instance.
+   */
+  public schema(method: HttpMethod, schema: (schema: typeof Joi) => ValidationSchemas): this {
+    if (this.schemas[method] === undefined)
+      this.schemas[method] = schema(Joi);
+    else
+      throw new Error(`Schemas for ${method} already exist!`);
+
+    return this;
   };
 
   /**
@@ -30,6 +77,11 @@ export default class RouteBuilder extends (EventEmitter as unknown as new () => 
       this.supportedMethods.push(event);
     else
       throw new Error(`Event ${event} already exists!`);
+
+    this.middleware = {
+      ...this.middleware,
+      [event]: [...(this.middleware?.[event] || []), this._validateSchema(event)],
+    };
 
     return super.on(event, listener);
   };
